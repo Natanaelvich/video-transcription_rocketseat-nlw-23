02b807feea5fc5ebc982587ba264374ae9735a47
natanaelvich <taelima1997@gmail.com>
2023-09-16 14:50:27 -0300

    add exclude ffmpeg for vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,9 +1,12 @@
+import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
