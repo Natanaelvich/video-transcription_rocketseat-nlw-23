a614e423b710af12432d25edddc90d489e1b94f7
natanaelvich <taelima1997@gmail.com>
2023-09-16 14:22:40 -0300

    add registers cors and routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,11 +1,23 @@
 import { fastify } from "fastify";
+import { fastifyCors } from '@fastify/cors'
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAiCompletionRoute } from "./routes/generate-ai-completion";
 
-const app = fastify();
+const app = fastify()
 
-app
-  .listen({
-    port: 3333,
-  })
-  .then(() => {
-    console.log("HTTP Server Running!");
-  });
+app.register(fastifyCors, {
+  origin: '*'
+})
+
+app.register(getAllPromptsRoute)
+app.register(uploadVideoRoute)
+app.register(createTranscriptionRoute)
+app.register(generateAiCompletionRoute)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server Running!')
+})
